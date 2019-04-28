f5b8dcd902989dc4c0379c4034676176f5be7d28
Prasun Jaiswal <prasunjais@gmail.com>
2019-04-28 23:50:06 +0530

    second

--- twilio.php
+++ twilio.php
@@ -2,8 +2,8 @@
 <?php
 require_once('Twilio/autoload.php');
 
-$sid = "ACa4112aa84b7cbe41489d2feb97d911f3"; // Your Account SID from www.twilio.com/console
-$token = "9852e30ac2c5b616eccd04d3488634a6"; // Your Auth Token from www.twilio.com/console
+$sid = ""; // Your Account SID from www.twilio.com/console
+$token = ""; // Your Auth Token from www.twilio.com/console
 
 $client = new Twilio\Rest\Client($sid, $token);
 try {
